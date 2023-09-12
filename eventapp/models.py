524c526990c55b51982904657ec31bc90c6d78b9
Arsha5502 <arshamjoby@gmail.com>
2023-09-12 22:32:14 +0530

    initial commit

---
+++ eventapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Event(models.Model):
+    event_title= models.CharField(max_length=120)
+    event_loc= models.CharField(max_length=70)
+    date = models.CharField(max_length=120)
+    description=models.TextField()
+    
