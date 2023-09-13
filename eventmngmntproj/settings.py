15ddaec383a990e321af3ed6d91723c8130e42af
Arsha5502 <arshamjoby@gmail.com>
2023-09-13 15:01:21 +0530

    initial commit

--- eventmngmntproj/settings.py
+++ eventmngmntproj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3t^-51iqpw-ljepl$-e2+hz^b0^=&whqnij=zk3u-or*%mt1ux'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
